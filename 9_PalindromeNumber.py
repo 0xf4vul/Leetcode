396e3bcb2851898a13693230ea17e8c658085403
AeroXi <31943024+AeroXi@users.noreply.github.com>
2019-01-28 11:57:39 +0800

    9

---
+++ 9_PalindromeNumber.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        array = []
+        y = x
+        while x != 0:
+            array.append(x % 10)
+            x = (x - x%10)/10
+        result = 0
+        i = 0
+        while(len(array) > 0):
+            result += array[-1]*(10**i)
+            array.pop()
+            i += 1
+        if y == result:
+            return True
+        return False
